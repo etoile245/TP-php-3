6b990ffeadc39d5b92018e6ad0127ff502289e6d
etoile245 <neomarischael@gmail.com>
2026-02-19 12:02:39 +0100

    vidéo 10

--- listeNationalites.php
+++ listeNationalites.php
@@ -5,7 +5,20 @@ $req=$monPdo->prepare("select * from nationalite");
 $req->setFetchMode(PDO::FETCH_OBJ);
 $req->execute();
 $lesNationalites=$req->fetchALL();
-var_dump($_SESSION)
+
+if(!empty($_SESSION['message'])){
+  $mesMessages=$_SESSION['message'];
+  foreach($mesMessages as $key=>$message){
+    echo '<div class="container pt-5">
+            <div class="alert alert-'.$key.' alert-dismissible fade show" role="alert">'.$message.'
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          </div>';
+  }
+  $_SESSION['message']=[];
+}
 ?>
 
 <div class="container mt-5">
